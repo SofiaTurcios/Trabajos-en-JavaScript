a25a7d5dd9badd666b0dd8520d9a89dde22534c5
SofiaTurcios <136513214+SofiaTurcios@users.noreply.github.com>
2024-07-17 14:34:31 -0600

    Add files via upload

---
+++ Trabajos en JavaScript/2. Variables/Variables.js
@@ -0,0 +1,7 @@
+//Variables globales: se usan repetidamete en el código
+var nombre = "Sofía López";
+
+//Variables locales
+let Código 
+
+document.write(nombre)
---
+++ Trabajos en JavaScript/3. Tipo de datos/Tipo de datos.js
@@ -0,0 +1,7 @@
+//Tipo de variables
+
+var numéricos = 5.25;
+var texto = "Bienvenidos";
+var booleanos = true;
+
+document.write(numéricos, "<br>", texto, "<br>", booleanos)
